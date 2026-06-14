445c44edb9784c17844d0e33fc44c27c27637a20
Eric Grana <scp00347@gmail.com>
2026-06-14 11:20:30 -0700

    Adding electron, removing python and switching backend to C#

--- frontend-react/public/electron.js
+++ frontend-react/public/electron.js
@@ -1,9 +1,10 @@
 const { app, BrowserWindow } = require('electron')
-
+const path = require('path')
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, 'icon.png'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
