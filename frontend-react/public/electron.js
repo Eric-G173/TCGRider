01733cdc178a6a025a316449d97e3c6e0fcd3006
Eric Grana <scp00347@gmail.com>
2026-06-16 23:31:26 -0700

    Removed top windows frame, making custom frame with minimize, zoom, and exit app buttons

--- frontend-react/public/electron.js
+++ frontend-react/public/electron.js
@@ -1,15 +1,22 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     icon: path.join(__dirname, 'icon.png'),
+     frame: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   })
+
+ ipcMain.on('window-minimize', () => win.minimize());
+  ipcMain.on('window-maximize', () => {
+    win.isMaximized() ? win.unmaximize() : win.maximize();
+  });
+  ipcMain.on('window-close', () => win.close());
 
   win.loadURL('http://localhost:3000')
   win.setMenuBarVisibility(false)
--- frontend-react/src/pages/Test.js
+++ frontend-react/src/pages/Test.js
@@ -1,29 +1,39 @@
-import logo from '../logo.svg';
+import logo from '../logo.png';
 import '../styles/Test.css';
-
-function MyButton() {
+// functions must start with capitalize letter
+function Button() {
   return (
     <button>I'm a button</button>
   )
 }
 
+function Logo() {
+  return (
+    <img src = {logo} alt ="EmailRider" className = "logo"/>
+  )
+}
+
+function AppName() {
+  return (
+    <p className = "AppName">EmailRider</p> //Elements must have a className in order to modify with css
+  )
+}
 function App() {
+  const { ipcRenderer } = window.require('electron');
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to no.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Bleh
-          <MyButton />
-        </a>
+        <Logo/> 
+          <AppName/>
+           <div className="drag-region" />
+        <div className="window-controls">
+          <button className="btn-minimize" onClick={() => ipcRenderer.send('window-minimize')}>─</button>
+
+       <button className="btn-maximize" aria-label="Maximize window" title="Maximize" onClick={() => ipcRenderer.send('window-maximize')}>
+  <div className="maximize-icon" />
+</button>
+         <button className="btn-close" onClick={() => ipcRenderer.send('window-close')}>✕</button>
+        </div>
       </header>
     </div>
   );
