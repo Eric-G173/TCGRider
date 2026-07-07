9dadd4aebce18f629d295edaa45d06d4eff4a1df
Eric Grana <egrana@icloud.com>
2026-07-06 18:56:07 -0700

    Fixed height issues causing scroll bar to go into task bar

--- frontend-react/public/electron.js
+++ frontend-react/public/electron.js
@@ -1,21 +1,42 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, screen } = require('electron')
 const path = require('path')
+
 function createWindow() {
+  const { workArea } = screen.getPrimaryDisplay();
+
   const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    x: workArea.x,
+    y: workArea.y,
+    width: workArea.width,
+    height: workArea.height,
+    useContentSize: true,
+    minWidth: 900,
+    minHeight: 600,
     icon: path.join(__dirname, 'icon.png'),
-     frame: false,
+    frame: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   })
 
- ipcMain.on('window-minimize', () => win.minimize());
+  let isMaximized = true; // starts true since we're opening at full workArea size
+  let previousBounds = null;
+
+  ipcMain.on('window-minimize', () => win.minimize());
+
   ipcMain.on('window-maximize', () => {
-    win.isMaximized() ? win.unmaximize() : win.maximize();
+    if (isMaximized) {
+      win.setBounds(previousBounds || { x: 100, y: 100, width: 1000, height: 700 });
+      isMaximized = false;
+    } else {
+      previousBounds = win.getBounds();
+      const { workArea } = screen.getPrimaryDisplay();
+      win.setBounds(workArea);
+      isMaximized = true;
+    }
   });
+
   ipcMain.on('window-close', () => win.close());
 
   win.loadURL('http://localhost:3000')
